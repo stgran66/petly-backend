ac608f92ffd2ab732a39450e5565104ddd1a5301
stgran66 <stanislaw.grygoriew@gmail.com>
2023-02-02 23:22:54 +0100

    installed some basic libraries, changed project name and file structure, wrote code for db connection

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = process.env.PORT || 3000;
+const uriDB = process.env.DB_HOST;
+
+mongoose.set('strictQuery', true);
+
+const connection = mongoose.connect(uriDB);
+
+connection
+  .then(() => console.log('Database connection successful'))
+  .catch((err) => {
+    console.log(`Cannot connect to database. Error message: ${err.message}`);
+    process.exit(1);
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((err) =>
+    console.log(`Server not running. Error message: ${err.message}`)
+  );
