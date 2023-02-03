3cca508b2c05d3f0e6d2167ebd4dab2034dae64e
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2023-02-03 22:29:08 +0200

    Swagger init
    Swagger init + edit readme.md(remove some garbage)

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const Router = require('./routes/');
+// add const for swagger
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
+const Router = require("./routes/");
 
 const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/', Router);
+app.use("/api/", Router);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
