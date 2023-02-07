5404521382682b28adc0729c044d6f01cf53720c
Ihor Podvalniy <bembasua@gmail.com>
2023-02-07 18:38:46 +0200

    Update listNoticesByCategory.js

--- controllers/notices/listNoticesByCategory.js
+++ controllers/notices/listNoticesByCategory.js
@@ -2,7 +2,6 @@ const { Notice } = require('../../models/notice');
 
 const listNotices = async (req, res, next) => {
   const { category } = req.params;
-  console.log(category);
   res.json(await Notice.find({ category }));
 };
 
