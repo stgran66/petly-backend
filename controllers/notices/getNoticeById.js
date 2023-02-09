26f62554bfcbedf2a61c540c45c6050629ef4860
Ihor Podvalniy <bembasua@gmail.com>
2023-02-09 21:09:25 +0200

    some minor bugfixes

--- controllers/notices/getNoticeById.js
+++ controllers/notices/getNoticeById.js
@@ -7,6 +7,7 @@ const getNoticeById = async (req, res, next) => {
     return res.status(404).json({ message: 'Not found' });
   }
   res.json(result);
+  next();
 };
 
 module.exports = getNoticeById;
