ba568225b5c322e2df2d3f2816ea2b42f6bdcca0
stgran66 <stanislaw.grygoriew@gmail.com>
2023-02-12 20:22:29 +0100

    added feature for getting current user

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -4,7 +4,8 @@ const signup = require('./signup');
 const login = require('./login');
 const logout = require('./logout');
 const updateAvatar = require('./updateAvatar');
-const updateUserData = require('../auth/updateUserData');
+const updateUserData = require('./updateUserData');
+const getCurrent = require('./getCurrent');
 
 module.exports = {
   signup: ctrlWrapper(signup),
@@ -12,4 +13,5 @@ module.exports = {
   logout: ctrlWrapper(logout),
   updateAvatar: ctrlWrapper(updateAvatar),
   updateUserData: ctrlWrapper(updateUserData),
+  getCurrent: ctrlWrapper(getCurrent),
 };
