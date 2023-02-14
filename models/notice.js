7592100eadccfa6720d5868208c02bfa2ed1fb2f
stgran66 <stanislaw.grygoriew@gmail.com>
2023-02-14 21:52:16 +0100

    fixed wrong notices validation

--- models/notice.js
+++ models/notice.js
@@ -110,10 +110,6 @@ const addNoticeValidation = (req, res, next) => {
         .max(24)
         .message('breed should be from 2 to 24 symbols'),
       place: Joi.string().min(4).max(60).required(),
-      price: Joi.string()
-        .min(1)
-        .pattern(/^[1-9][0-9]*$/)
-        .message('price cannot starts from zero'),
       name: Joi.string()
         .min(2)
         .max(16)
