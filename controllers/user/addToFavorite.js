5b46ca770f44fe3170c943b88356b8689cb0ccb7
stgran66 <stanislaw.grygoriew@gmail.com>
2023-02-15 11:07:13 +0100

    added check if notice is already in favorites

--- controllers/user/addToFavorite.js
+++ controllers/user/addToFavorite.js
@@ -4,8 +4,12 @@ const addToFavorite = async (req, res, next) => {
   const { noticeId } = req.params;
   const { _id: owner } = req.user;
 
+  const user = await User.findById(owner);
+  user.favorite.includes(noticeId) &&
+    res.status(400).json({ message: 'notice already in favorite' });
+
   await User.findByIdAndUpdate(owner, {
-    $push: { favorite: noticeId },
+    $addToSet: { favorite: noticeId },
   });
   res.json(noticeId);
 };
