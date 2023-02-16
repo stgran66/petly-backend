0af62257da919a805f33ee4676b69e74bc975559
Ihor Podvalniy <bembasua@gmail.com>
2023-02-16 19:13:47 +0200

    adds pagination

--- controllers/notices/listNoticesByCategory.js
+++ controllers/notices/listNoticesByCategory.js
@@ -2,12 +2,14 @@ const { Notice } = require('../../models/notice');
 
 const listNotices = async (req, res, next) => {
   const { category } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
   if (
     category === 'sell' ||
     category === 'lost-found' ||
     category === 'for-free'
   ) {
-    const notices = await Notice.find({ category });
+    const notices = await Notice.find({ category }, {}, { skip, limit });
 
     return res.json(notices);
   }
--- controllers/user/listFavorite.js
+++ controllers/user/listFavorite.js
@@ -2,7 +2,14 @@ const { User } = require('../../models/user');
 
 const listFavorite = async (req, res, next) => {
   const { id: owner } = req.user;
-  res.json(await User.findById(owner, 'favorite').populate('favorite'));
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  res.json(
+    await User.findById(owner, 'favorite').populate({
+      path: 'favorite',
+      options: { skip: skip, limit: limit },
+    })
+  );
 };
 
 module.exports = listFavorite;
