f44d56bf126b96073ba26115e13751134298dd9a
stgran66 <stanislaw.grygoriew@gmail.com>
2023-02-20 20:28:24 +0100

    added query for notices in categories

--- controllers/notices/listNoticesByCategory.js
+++ controllers/notices/listNoticesByCategory.js
@@ -2,15 +2,37 @@ const { Notice } = require('../../models/notice');
 
 const listNotices = async (req, res, next) => {
   const { category } = req.params;
-  const { page = 1, limit = 12 } = req.query;
+  const { page = 1, limit = 12, query = '' } = req.query;
   const skip = (page - 1) * limit;
   if (
     category === 'sell' ||
     category === 'lost-found' ||
     category === 'for-free'
   ) {
-    const allNotices = await Notice.find({ category });
-    const notices = await Notice.find({ category }, {}, { skip, limit }).sort({
+    const allNotices = await Notice.find({
+      category,
+      $or: [
+        { comments: { $regex: query, $options: 'i' } },
+        { title: { $regex: query, $options: 'i' } },
+        { name: { $regex: query, $options: 'i' } },
+        { breed: { $regex: query, $options: 'i' } },
+        { place: { $regex: query, $options: 'i' } },
+      ],
+    });
+    const notices = await Notice.find(
+      {
+        category,
+        $or: [
+          { comments: { $regex: query, $options: 'i' } },
+          { title: { $regex: query, $options: 'i' } },
+          { name: { $regex: query, $options: 'i' } },
+          { breed: { $regex: query, $options: 'i' } },
+          { place: { $regex: query, $options: 'i' } },
+        ],
+      },
+      {},
+      { skip, limit }
+    ).sort({
       createdAt: -1,
     });
 
